26bd3c0e385e88879c12628e2d8201b29b12d832
Rayen Bouhoula <rayen.bouhoula@musteducation.tn>
2026-02-10 19:02:43 +0100

    kj

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Skills from './components/Skills'
 import GitHubStats from './components/GitHubStats'
-import Achievements from './components/Achievements'
 import Projects from './components/Projects'
 import Blog from './components/Blog'
 import Contact from './components/Contact'
@@ -70,7 +69,6 @@ const AppContent = () => {
       <About />
       <Skills />
       <GitHubStats />
-      <Achievements />
       <Projects />
       <Blog />
       <Contact />
